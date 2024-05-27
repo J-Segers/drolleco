b8f73fa15fea890be4bcc6114d0a2c3a586a724a
J-Segers <jethrosegers@gmail.com>
2024-05-27 14:44:01 +0200

    added assets folder and created a basic hero

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import heroImg from "../../assets/New folder/AdobeStock_214508643.jpeg";
+import "./Hero.css";
+
+function Hero() {
+    return (
+        <div id="hero">
+            <img src={heroImg} />
+        </div>
+    );
+}
+
+export default Hero;
